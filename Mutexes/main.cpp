a6e1ab1f30ed5a92c6ae5a65e06c251e3c4f84dd
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-09-28 23:43:23 +0200

    Before using mutex

--- Mutexes/main.cpp
+++ Mutexes/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Concurrency-programing
+//  Mutexes
 //
 //  Created by Imad Collin on 2017-09-28.
 //  Copyright © 2017 Imad Collin. All rights reserved.
@@ -9,25 +9,28 @@
 #include <iostream>
 #include <thread>
 using namespace std;
+int x;
 class C{
 public:
     void operator()(){
-        cout<<"Method from the class "<<endl;
+        for(int i=0;i<100;i++){
+            cout<<i<<endl;
+        }
+        
     }
 };
 
-void function_out(){
-    cout<<"Method outside the class"<<endl;
-}
 int main(int argc, const char * argv[]) {
     C c;
+    
     thread t(c);
-    thread t2(function_out);
+    for(int i=0;i<100;i++){
+        cout<<i<<endl;
+    }
+    
     if(t.joinable())
         t.join();
-    if(t2.joinable())
-        t2.join();
-    
     
     return 0;
 }
+
